502fd7c77e0d186d843b212e7d5c3f9c0f1e2c01
Paul Matchett <p.matchett80@gmail.com>
2018-06-05 15:29:28 +0100

    add test for get todos

--- 05-node-todo-api/server/tests/server.test.js
+++ 05-node-todo-api/server/tests/server.test.js
@@ -4,14 +4,25 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos = [
+    {
+        text: 'first test todo'
+    },
+    {
+        text: 'second test todo'
+    }
+]
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => done());
 });
 
 describe('POST / todos', () => {
+
     it('should create a new todo', (done) => {
         var text = 'Test todo test';
-
         request(app)
             .post('/todos')
             .send({text})
@@ -23,9 +34,8 @@ describe('POST / todos', () => {
                 if(error) {
                     return done(error);
                 }
-
                 Todo
-                    .find()
+                    .find({text})
                     .then((todos) => {
                         expect(todos.length).toBe(1);
                         expect(todos[0].text).toBe(text);
@@ -44,14 +54,28 @@ describe('POST / todos', () => {
                 if(error) {
                     return done(error);
                 }
-
                 Todo
                     .find()
                     .then((todos) => {
-                        expect(todos.length).toBe(0);
+                        expect(todos.length).toBe(2);
                         done();
                     })
                     .catch((error) => done(error))
             });
     });
-})
+
+});
+
+describe('GET / todos', () => {
+
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
+    
+});
