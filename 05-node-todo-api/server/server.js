e94b0fac01966b3a348cf92fe7b48daffb3b8e9b
Paul Matchett <p.matchett80@gmail.com>
2018-06-05 16:56:27 +0100

    added get todo by id

--- 05-node-todo-api/server/server.js
+++ 05-node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { User } = require('./models/user');
@@ -27,69 +28,31 @@ app.get('/todos', (req, res) => {
     Todo.find()
         .then((todos) => {
             res.send({ todos });
-        }, (e) => {
+        }, (error) => {
             res.status(400).send(error);
-            console.log('Unable to save todo');
         });
 });
 
-// app.post('/users', (req, res) => {
-//     var newUser = new User(req.body)
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
 
-//     newUser.save().then((doc) => {
-//         console.log(JSON.stringify(doc, undefined, 2));
-//         res.send(doc);
-//     }, (error) => {
-//         res.status(400).send(error);
-//         console.log('Unable to save user');
-//     });
+    if(!ObjectID(id)){
+        return res.status(404).send();
+    }
 
-// });
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(400).send();
+        }
+        res.send({todo});
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
 
-// app.get('/todos', (req, res) => {
-
-// });
-
-// app.get('/todo:id', (req, res) => {
-
-// });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
-})
-
-// const newUser = (email) => {
-
-//     var newUser = new User({
-//         email
-//     })
-
-//     newUser.save().then((doc) => {
-//         console.log(JSON.stringify(doc, undefined, 2));
-//     }, (error) => {
-//         console.log('Unable to save user');
-//     });
-
-// }
-
-// const newTodo = (todo) => {
-
-//     var newTodo = new Todo(todo)
-
-//     newTodo.save().then((doc) => {
-//         console.log(JSON.stringify(doc, undefined, 2));
-//     }, (error) => {
-//         console.log('Unable to save todo');
-//     });
-
-// }
-
-// var collection = 'user';
-
-// if(collection == 'user'){
-//     newUser('emailthree@email.com');
-// } else {
-//     newTodo({ text: 'Bath da ba', completed: false, completedAt: 123 });
-// }
+});
 
 module.exports = { app }
