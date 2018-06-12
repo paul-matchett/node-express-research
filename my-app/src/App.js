85d50b501c4c8a2ecf792f91bac5f846cf9fbe54
Paul Matchett <p.matchett80@gmail.com>
2018-06-12 16:58:21 +0100

    redux added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+import Layout from './hoc/Layout/Layout';
+import Logout from './containers/Auth/Logout/Logout';
+import Todos from './containers/Todos/Todos';
+import * as actions from './store/actions/index';
+
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth');
+});
 
 class App extends Component {
+
+  componentDidMount () {
+    this.props.onTryAutoSignup();
+  }
+
   render() {
+
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={asyncAuth} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if ( this.props.isAuthenticated ) {
+      routes = (
+        <Switch>
+          <Route path="/" component={Todos} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/auth" component={asyncAuth} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Layout>
+          {routes}
+        </Layout>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch( actions.authCheckState() )
+  };
+};
+
+export default withRouter( connect( mapStateToProps, mapDispatchToProps )( App ) );
---
+++ my-app/src/store/actions/auth.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: token,
+        userId: userId
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
+
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+        };
+        let url = 'http://localhost:5000/users';
+        if (!isSignup) {
+            url = 'http://localhost:5000/users/login';
+        }
+        console.log('url :', url);
+        axios.post(url, authData)
+            .then(response => {
+              console.log('response :', response);
+              localStorage.setItem('token', response.data.token);
+              localStorage.setItem('userId', response.data.user._id);
+              dispatch(authSuccess(response.data.idToken, response.data.user._id));
+            })
+            .catch(err => {
+                dispatch(authFail(err.response.data.error));
+            });
+    };
+};
+
+export const setAuthRedirectPath = (path) => {
+    return {
+        type: actionTypes.SET_AUTH_REDIRECT_PATH,
+        path: path
+    };
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+          const userId = localStorage.getItem('userId');
+          dispatch(authSuccess(token, userId));
+        }
+    };
+};
